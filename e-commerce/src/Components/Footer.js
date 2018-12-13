fcd4a7f2b380b183752ee552c38a28217e2521f2
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-12 20:36:17 -0700

    Footer

---
+++ e-commerce/src/Components/Footer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from "react-router-dom"
+import "../ComponentStyles/Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="first-footer-section">
+                <div className="follow-us">
+                    <h2>Follow Us:</h2>
+                    <div className="brands">
+                        <p><FontAwesomeIcon icon={['fab', 'facebook']} /></p>
+                        <p><FontAwesomeIcon icon={['fab', 'instagram']} /></p>
+                        <p><FontAwesomeIcon icon={['fab', 'linkedin']} /></p>
+                        <p><FontAwesomeIcon icon={['fab', 'twitter']} /></p>
+                    </div>
+                    <hr/>
+                </div>
+                <div className="quick-links">
+                    <h2>Quick Services:</h2>
+                    <div className="links">
+                        <Link to="/">FAQ</Link>
+                        <Link to="/">Return</Link>
+                        <Link to="/">Shipping</Link>
+                        <Link to="/">Privacy Policy</Link>
+                        <Link to="/">Blog</Link>
+                    </div>
+                </div>
+                <p className="copy-right">Copyright@ 2018 Decor's <i>Duc</i> </p>
+            </div>
+            <div className="second-footer-section">
+                <div className="new-sletter">
+                    <h2>Sign up to get discount!!</h2>
+                    <div className = "sign-up-email">
+                        <input placeholder="Email Address"></input>
+                        <button>SIGN UP</button>
+                    </div>
+                </div>
+                <div className="help">
+                    <h2>Need help? Call Us:</h2>
+                    <h2>18822712811</h2>
+                </div>
+                <div className="credits-card">
+                    <div className= "credit-cards">
+                    <p><FontAwesomeIcon icon={['fab', 'cc-visa']} /></p>
+                    <p><FontAwesomeIcon icon={['fab', 'cc-stripe']} /></p>
+                    <p><FontAwesomeIcon icon={['fab', 'cc-paypal']} /></p>
+                    <p><FontAwesomeIcon icon={['fab', 'cc-discover']} /></p>
+                    <p><FontAwesomeIcon icon={['fab', 'cc-amex']} /></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./Components/App";
 import "./ComponentStyles/styles.css"
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faSearch,faShoppingCart, faTimes, faCircle, faHeart, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { fab} from '@fortawesome/free-brands-svg-icons'
-// import { BrowserRouter } from "react-router-dom";
+import { faBars, faSearch, faShoppingCart, faTimes, faCircle, faHeart, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { BrowserRouter } from "react-router-dom";
 library.add(faArrowLeft)
 library.add(faArrowRight)
 library.add(faHeart)
@@ -17,6 +16,7 @@ library.add(faBars)
 library.add(faTimes)
 library.add(faCircle)
 ReactDOM.render(
-
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
     , document.getElementById("root"))
