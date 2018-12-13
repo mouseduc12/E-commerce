7e0d86aa37b62edcd64af6e5299636b074b36de2
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-13 00:34:30 -0700

    garden Schema

---
+++ models/gardenSculpture.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const gardenSculptureSchema = new Schema({
+    headline: {
+        type: String,
+        required: true
+    },
+    price: Number,
+    image: String,
+    otherImages: {
+        type: Array,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }, 
+    relatedProduct: {
+        type: [{
+            type: Object 
+        }]
+    }
+})
+
+module.exports = mongoose.model("gardenSculpture", gardenSculptureSchema)
