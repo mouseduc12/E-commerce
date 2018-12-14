ac5d3cb2d37b70fb918356846feb54be98b7dbb0
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-14 16:58:06 -0700

    add animation

--- e-commerce/src/Components/GeneralShop.js
+++ e-commerce/src/Components/GeneralShop.js
@@ -4,18 +4,47 @@ import ShowOffProduct from "./ShowOffProduct";
 import { withProduct } from "../Context/ProductsProvider"
 import "../ComponentStyles/GeneralShop.css"
 
+
 class GeneralShop extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            scrollX: false
+        }
+        this.myRef = React.createRef()
+    }
     componentDidMount() {
-        this.props.getPlant()
+        this.props.getPlant();
+        window.addEventListener("scroll", this.handleScroll)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll)
+    }
+    handleScroll = () => {
+        const manageShowOff = this.myRef.current
+        if (manageShowOff.scrollLeft > 100) {
+            this.setState({
+                scrollX: true
+            })
+        } else {
+            this.setState({
+                scrollX: false
+            })
+        }
     }
     render() {
-        console.log(this.props.plants)
         return (
             <div>
                 <Events />
-                <div className="manage-showoff">
-                    <div className = "manage-store-teller">
-                        <div className="visit-store-teller">
+                <div className="manage-showoff" onScroll={this.handleScroll} ref={this.myRef}>
+                    <div className="manage-store-teller">
+                        <div className="visit-store-teller" style={{
+                            position: this.state.scrollX && "absolute",
+                            backgroundColor: this.state.scrollX && "#41474d",
+                            color: this.state.scrollX && "white",
+                            opacity: this.state.scrollX && 1
+                        }}>
                             <p>VISIT OUR</p>
                             <h2>PLANTS</h2>
                         </div>
--- e-commerce/src/Components/ShowOffProduct.js
+++ e-commerce/src/Components/ShowOffProduct.js
@@ -1,18 +1,63 @@
 import React from "react"
 import "../ComponentStyles/ShowOffProduct.css"
+import LazyLoad from "react-lazyload"
 
-const ShowOffProduct = (props) => {
-    return (
-        <div className="manage-front">
-            <div className="manage-front-inside">
-                <div style={{ backgroundImage: `url(${props.image})` }} className="product-image"></div>
-                <div className = "product-image-text">
-                    <h3>{props.headline}</h3>
-                    <p>{props.price}</p>
+class ShowOffProduct extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            mouseEnter: false,
+            mouseLeave: true,
+            slideOver: 0
+        }
+        this.intervalId = undefined
+    }
+
+    handleOver = () => {
+        this.intervalId = setInterval(
+            () => {
+                if (this.state.slideOver >= this.props.otherImages.length - 1) {
+                    console.log("It's right")
+                    this.setState({
+                        slideOver: 0
+                    })
+                }
+                else {
+                    this.setState(prevState => ({
+                        slideOver: prevState.slideOver + 1,
+                        mouseEnter: true,
+                        mouseLeave: false
+                    }))
+                }
+            }, 2000)
+    }
+    handleLeave = () => {
+        clearInterval(this.intervalId)
+        this.setState({
+            mouseLeave: true,
+            mouseEnter: false
+        })
+    }
+
+    render() {
+        return (
+            <LazyLoad height={400} once throttle={1000}>
+                <div className="manage-front">
+                    <div className="manage-front-inside">
+                        <div
+                            onMouseEnter={this.handleOver}
+                            onMouseLeave={this.handleLeave}
+                            style={{ backgroundImage: this.state.mouseEnter ? `url(${this.props.otherImages[this.state.slideOver]})` : `url(${this.props.image})` }}
+                            className="product-image">
+                        </div>
+                        <div className="product-image-text">
+                            <h3>{this.props.headline}</h3>
+                            <p>{this.props.price}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            </LazyLoad>
+        )
+    }
 }
-
 export default ShowOffProduct   
