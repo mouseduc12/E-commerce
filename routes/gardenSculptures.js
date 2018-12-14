403783c171610d9d29a2a7b0ce204ada16fcf0c9
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-13 21:01:12 -0700

    sculpture route

---
+++ routes/gardenSculptures.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const sculptureRouter = express.Router()
+const gardenSculpture = require("../models/gardenSculpture")
+
+sculptureRouter.get("/", (req, res, next) => {
+    gardenSculpture.find((err, data) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(data)
+    })
+})
+
+sculptureRouter.post("/", (req, res, next) => {
+    const newSculpture = new gardenSculpture(req.body)
+    newSculpture.save((err, data) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(data)
+    })
+})
+
+module.exports = sculptureRouter;
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose")
 
 app.use(express.json())
 app.use(morgan("dev"))
-app.use("/outdoorLight", require("./routes/manSuits"))
+app.use("/plants", require("./routes/plants"))
+app.use("/sculptures", require("./routes/plants"))
 
 app.use((err, req, res, next) =>{
     return res.status(500).send({error: err.message})
