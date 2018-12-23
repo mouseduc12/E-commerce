b5fd599c3d229408eddad2377ab234ef13ac54ed
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-23 13:10:10 -0700

    add functionalities

--- e-commerce/src/Components/User/User.js
+++ e-commerce/src/Components/User/User.js
@@ -3,31 +3,89 @@ import { withAuth } from "../../Context/AuthContext";
 import "../../ComponentStyles/User.css"
 
 class User extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            userInfoChecked: true,
+            changePassword: false,
+            orders: false,
+            writeArticle: false
+        }
+    }
+
+    handleInfoClick = () => {
+        this.setState({
+            userInfoChecked: true,
+            changePassword: false,
+            orders: false,
+            writeArticle: false
+        })
+    }
+
+    handleChangePassword = () => {
+        this.setState({
+            userInfoChecked: false,
+            changePassword: true,
+            orders: false,
+            writeArticle: false
+        })
+    }
+
+    handleOrders = () => {
+        this.setState({
+            userInfoChecked: false,
+            changePassword: false,
+            orders: true,
+            writeArticle: false
+        })
+    }
+
+    handleWriteArticle = () => {
+        this.setState({
+            userInfoChecked: false,
+            changePassword: false,
+            orders: false,
+            writeArticle: true
+        })
+    }
+
     render() {
-        console.log(this.props.user)
         return (
             <div className="user">
                 <div
                     style={{ backgroundImage: this.props.user.faceImage ? `url(${this.props.user.faceImage})` : `url("https://res.cloudinary.com/hd1n2hd4y/image/upload/f_auto,q_auto,c_fill,dpr_2.0,w_512,h_512,g_face/user-default.jpg")` }}
                     className="user-image">
                 </div>
-                <div className="user-infos">
-                    <p>User Info</p>
-                </div>
-                <div className="change-password">
-                    <p>Change Password</p>
-                </div>
-                <div className = "orders">
-                    <p>Ordered</p>
-                </div>
-                <div className = "log-out">
-                    <p>Log Out</p>
-                </div>
-                {this.props.user.isAdmin &&
-                    <div className="write-article">
-                        
+                <div>
+                    <div
+                        className="user-infos choose"
+                        style={{ color: this.state.userInfoChecked && "white", backgroundColor: this.state.userInfoChecked && "#232f3e" }}
+                    >
+                        <p onClick={this.handleInfoClick}>User Info</p>
                     </div>
-                }
+                    <div
+                        className="change-password choose"
+                        style={{ color: this.state.changePassword && "white", backgroundColor: this.state.changePassword && "#232f3e" }}
+                    >
+                        <p onClick={this.handleChangePassword}>Change Password</p>
+                    </div>
+                    <div
+                        className="orders choose"
+                        style={{ color: this.state.orders && "white", backgroundColor: this.state.orders && "#232f3e" }}>
+                        <p onClick={this.handleOrders}>Ordered</p>
+                    </div>
+                    <div className="log-out choose">
+                        <p onClick={this.props.logout}>Log Out</p>
+                    </div>
+                    {this.props.user.isAdmin &&
+                        <div
+                            className="write-article"
+                            style={{ color: this.state.writeArticle && "white", backgroundColor: this.state.writeArticle && "#232f3e" }}
+                        >
+                            <p onClick={this.handleWriteArticle}>Write Article</p>
+                        </div>
+                    }
+                </div>
             </div>
         )
     }
--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./Components/App";
 import "./ComponentStyles/styles.css"
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faSearch, faShoppingCart, faTimes, faCircle, faHeart, faArrowRight, faArrowLeft, faTruck, faPlaneDeparture, faGift, faPhone, faLongArrowAltRight, faLongArrowAltLeft, faQuoteLeft, faMapMarkerAlt, faEnvelope, faArrowDown, faArrowUp, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSearch, faShoppingCart, faTimes, faCircle, faHeart, faArrowRight, faArrowLeft, faTruck, faPlaneDeparture, faGift, faPhone, faLongArrowAltRight, faLongArrowAltLeft, faQuoteLeft, faMapMarkerAlt, faEnvelope, faArrowDown, faArrowUp, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import ProductsProvider from "./Context/ProductsProvider";
 import AuthContext from "./Context/AuthContext"
 import { BrowserRouter } from "react-router-dom";
-library.add(faArrowLeft, faTruck, faPlaneDeparture, faGift, faPhone, faLongArrowAltRight, faLongArrowAltLeft, faQuoteLeft, faMapMarkerAlt, faEnvelope, faArrowDown, faArrowUp, faUser)
+library.add(faArrowLeft, faTruck, faPlaneDeparture, faGift, faPhone, faLongArrowAltRight, faLongArrowAltLeft, faQuoteLeft, faMapMarkerAlt, faEnvelope, faArrowDown, faArrowUp, faUser, faSignOutAlt)
 library.add(faArrowRight)
 library.add(faHeart)
 library.add(fab)
