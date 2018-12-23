6c307358d3a099bd71c052548a63ef2e7a8fa236
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-23 12:26:46 -0700

    add user component

--- e-commerce/src/Components/User/User.js
+++ e-commerce/src/Components/User/User.js
@@ -1,11 +1,33 @@
 import React from "react"
-import { withAuth } from "../../Context/AuthContext"
+import { withAuth } from "../../Context/AuthContext";
+import "../../ComponentStyles/User.css"
 
-class User extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>Hello world</h2>
+class User extends React.Component {
+    render() {
+        console.log(this.props.user)
+        return (
+            <div className="user">
+                <div
+                    style={{ backgroundImage: this.props.user.faceImage ? `url(${this.props.user.faceImage})` : `url("https://res.cloudinary.com/hd1n2hd4y/image/upload/f_auto,q_auto,c_fill,dpr_2.0,w_512,h_512,g_face/user-default.jpg")` }}
+                    className="user-image">
+                </div>
+                <div className="user-infos">
+                    <p>User Info</p>
+                </div>
+                <div className="change-password">
+                    <p>Change Password</p>
+                </div>
+                <div className = "orders">
+                    <p>Ordered</p>
+                </div>
+                <div className = "log-out">
+                    <p>Log Out</p>
+                </div>
+                {this.props.user.isAdmin &&
+                    <div className="write-article">
+                        
+                    </div>
+                }
             </div>
         )
     }
