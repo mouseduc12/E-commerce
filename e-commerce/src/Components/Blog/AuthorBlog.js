f80a04b089c31fa2bc16a5d97b15651290a243ac
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-25 17:28:25 -0700

    finished author post

--- e-commerce/src/Components/Blog/AuthorBlog.js
+++ e-commerce/src/Components/Blog/AuthorBlog.js
@@ -1,17 +1,21 @@
 import React from "react"
 import moment from "moment"
+import "../../ComponentStyles/AuthorBlog.css"
+import { Link } from "react-router-dom"
 
 const AuthorBlog = (props) => {
     return (
-        <div className="feature-of-author-each-blog">
-            <div className="feature-image-of-each-blog" style={{ backgroundImage: props.featureImage }}>
+        <Link to = {`/blog/${props.user._id}/${props._id}`} style ={{color: "black"}}>
+            <div className="feature-of-author-each-blog">
+                <div className="feature-image-of-each-blog" style={{ backgroundImage: `url(${props.featureImage})` }}>
+                </div>
+                <div className="feature-content-of-author-blog">
+                    <h2>{props.title}</h2>
+                    {props.firstContent.length > 50 ? <p>{props.firstContent.slice(0, 50) + "[...]"}</p> : <p>{props.firstContent}</p>}
+                    <p>{moment(props.Date).format("llll")}</p>
+                </div>
             </div>
-            <div className = "feature-content-of-author-blog">
-                <h2>{props.title}</h2>
-                {props.firstContent.length > 50 ? <p>{props.firstContent.slice(0, 50) + "[...]"}</p> : <p>{props.firstContent}</p>}
-                <p>{moment(props.Date).format("llll")}</p>
-            </div>
-        </div>
+        </Link>
     )
 }
 
--- e-commerce/src/Components/Blog/AuthorBlogs.js
+++ e-commerce/src/Components/Blog/AuthorBlogs.js
@@ -1,18 +1,37 @@
 import React from "react"
 import { withBlog } from "../../Context/CreateBlog"
 import AuthorBlog from "./AuthorBlog"
+import "../../ComponentStyles/AuthorBlogs.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class AuthorBlogs extends React.Component{
-    componentDidMount(){
+class AuthorBlogs extends React.Component {
+    componentDidMount() {
         this.props.handleAuthorInfos(this.props.match.params.userId)
     }
 
-    render(){
-        return(
-            <div>
-                {
-                    this.props.aboutAuthorData.map(each => <AuthorBlog {...each}/>)
-                }
+    render() {
+        return (
+            <div className="about-blog-of-author-big-container">
+                <div className="about-blog-of-author-container">
+                    {this.props.aboutAuthorData.length >= 1 &&
+                        <div className="small-container-of-author">
+                            <div className="author-of-these-blogs"
+                                style={{ backgroundImage: `url(${this.props.aboutAuthorData[0].user.faceImage})` }}>
+                            </div>
+                            <h2>{this.props.aboutAuthorData[0].user.firstName}</h2>
+                            <div className = "icon-for-posts">
+                                <p><FontAwesomeIcon icon="signal" /></p>
+                                <p>{this.props.aboutAuthorData.length} posts</p>
+                            </div>
+                            <hr />
+                        </div>
+                    }
+                    <div className="feature-of-author-each-blog-container">
+                        {
+                            this.props.aboutAuthorData.reverse().map(each => <AuthorBlog {...each} />)
+                        }
+                    </div>
+                </div>
             </div>
         )
     }
